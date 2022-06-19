65822fdac88fa125356b2c563ea9292422811a3d
AntonioP22 <mammothsnowb@gmail.com>
2022-06-19 16:22:22 -0700

    Update dbconn

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$serverName = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "project";
+$serverName = "us-cdbr-east-05.cleardb.net";
+$dBUsername = "b2cc4eb6372415";
+$dBPassword = "f4c74845";
+$dBName = "heroku_05ed115d2962d3f";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
