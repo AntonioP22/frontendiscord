546a737b6ee133914cdbfcdefe5bf5ace2be65c6
joppe montezinos <34985258+joppe2001@users.noreply.github.com>
2022-06-20 17:47:24 +0200

    test commit from joppe

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
   <body>
     <nav>
       <ul>
-        <li><a href="index.php">Home</a></li>
+        <li><a href="index.php">Home test</a></li>
         <li><a href="discover.php">About Us</a></li>
         <?php
           if (isset($_SESSION['userUid'])) {
