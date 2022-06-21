2599a23982670175efa7725f805b762a919bae29
AntonioP22 <mammothsnowb@gmail.com>
2022-06-21 06:16:01 -0700

    recommit

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
     Coded by the Frontend Discord Community
-    <img src="https://pngimg.com/uploads/github/github_PNG58.png" title="Click Me"/>
+    <embed src="https://pngimg.com/uploads/github/github_PNG58.png" title="Click Me"/>
   </footer>
 </body>
 </html>
