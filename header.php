5d4fc2ced734abb65379ba0f0406b083914d46de
AntonioP22 <mammothsnowb@gmail.com>
2022-06-21 06:48:45 -0700

    update

--- header.php
+++ header.php
@@ -19,31 +19,7 @@ session_start();
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/comic-mono@0.0.1/index.css">
 </head>
 
-<body>
-  <nav>
-    <ul class="navbar-nav">
-      <li>
-        <a href="index.php">Home</a>
-      </li>
-      <li>
-        <a href="projects.php">Community Projects</a>
-      </li>
-      <li>
-        <a href="discover.php">About Us</a>
-      </li>
-      <?php
-      if (isset($_SESSION['userUid'])) {
-        echo '<li><a href="profile.php">Profile Page</a></li>';
-        echo '<li><a href="includes/logout.inc.php">Log out</a></li>';
-      } else {
-        echo '<li><a href="signup.php">Sign up</a></li>';
-        echo '<li><a href="login.php">Log in</a></li>';
-      }
-      ?>
-    </ul>
-  </nav>
-
-  
+<body>  
   <header>
     <nav>
       <ul class="navbar-nav">
