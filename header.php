36a52f34912e42a74a55f9ae5b7ccde97166b92b
joppe montezinos <34985258+joppe2001@users.noreply.github.com>
2022-06-21 15:10:09 +0200

    change in css

--- header.php
+++ header.php
@@ -1,55 +1,44 @@
 <?php
- session_start();
+session_start();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Frontend Discord Community</title>
-    <link rel="stylesheet" href="/design/reset.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Italiana&display=swap"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="/resources/css/style.css">
-    <!-- CSS only -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/comic-mono@0.0.1/index.css">
-  </head>
 
-  <body>
-    <nav class="navbar navbar-expand-lg">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">FEDC</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Community Projects</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="discover.php">About Us</a>
-        </li>
-        <?php
-          if (isset($_SESSION['userUid'])) {
-            echo '<li><a href="profile.php">Profile Page</a></li>';
-            echo '<li><a href="includes/logout.inc.php">Log out</a></li>';
-          }
-          else {
-            echo '<li><a href="signup.php">Sign up</a></li>';
-            echo '<li><a href="login.php">Log in</a></li>';
-          }
-        ?>
-      </ul>
-    </div>
-  </div>
-</nav>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Frontend Discord Community</title>
+  <link rel="stylesheet" href="/design/reset.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Italiana&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="/resources/css/style.css">
+  <!-- CSS only -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/comic-mono@0.0.1/index.css">
+</head>
+
+<body>
+  <nav>
+    <ul class="navbar-nav">
+      <li>
+        <a href="index.php">Home</a>
+      </li>
+      <li>
+        <a href="#">Community Projects</a>
+      </li>
+      <li>
+        <a href="discover.php">About Us</a>
+      </li>
+      <?php
+      if (isset($_SESSION['userUid'])) {
+        echo '<li><a href="profile.php">Profile Page</a></li>';
+        echo '<li><a href="includes/logout.inc.php">Log out</a></li>';
+      } else {
+        echo '<li><a href="signup.php">Sign up</a></li>';
+        echo '<li><a href="login.php">Log in</a></li>';
+      }
+      ?>
+    </ul>
+  </nav>
