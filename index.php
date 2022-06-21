a204c3c7f6a175c0e4fb8c2b11b0aa86b202c796
joppe montezinos <34985258+joppe2001@users.noreply.github.com>
2022-06-21 21:39:34 +0200

    testing smt

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php
-    include_once 'header.php';
+include_once 'header.php';
 ?>
 
-        <?php
-          if (isset($_SESSION['userUid'])) {
-            echo "<p>You are logged in " . $_SESSION["userUid"] . "</p>";
-          }
-        ?>
-    <section class="mid-section">
-      <h2>Welcome to the our open-source project!</h2>
-      <article>
-        <p>If you'd like to contribute please read GitHub's contributing rules and guidelines. Then reach out with either a pull request or username to become a contributor!</p>
-        
-      </article>
-    </section>
+
+<section class="mid-section">
+<?php
+if (isset($_SESSION['userUid'])) {
+  echo "<p>You are logged in " . $_SESSION["userUid"] . "</p>";
+}
+?>
+  <h2>Welcome to the our open-source project!</h2>
+  <article>
+    <p>If you'd like to contribute please read GitHub's contributing rules and guidelines. Then reach out with either a pull request or username to become a contributor!</p>
+
+  </article>
+</section>
 
 <?php
-    include_once 'footer.php';
+include_once 'footer.php';
 ?>
