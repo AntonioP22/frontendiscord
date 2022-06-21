7567ed93435c71db068eb0d499d90fda1052a62b
joppe montezinos <34985258+joppe2001@users.noreply.github.com>
2022-06-21 21:45:25 +0200

    testr

--- index.php
+++ index.php
@@ -4,16 +4,17 @@ include_once 'header.php';
 
 
 <section class="mid-section">
-<?php
-if (isset($_SESSION['userUid'])) {
-  echo "<p>You are logged in " . $_SESSION["userUid"] . "</p>";
-}
-?>
+
   <h2>Welcome to the our open-source project!</h2>
   <article>
     <p>If you'd like to contribute please read GitHub's contributing rules and guidelines. Then reach out with either a pull request or username to become a contributor!</p>
 
   </article>
+  <?php
+if (isset($_SESSION['userUid'])) {
+  echo "<p>You are logged in " . $_SESSION["userUid"] . "</p>";
+}
+?>
 </section>
 
 <?php
