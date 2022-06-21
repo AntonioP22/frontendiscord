0931ea11c2507219e165f6d3fd1b94c98a8acab5
AntonioP22 <mammothsnowb@gmail.com>
2022-06-20 23:09:31 -0700

    Create About page

---
+++ discover.php
@@ -0,0 +1,11 @@
+<?php
+    include_once 'header.php';
+?>
+<h1>Frontend Discord Community</h1>
+<hr/>
+<section>
+  <h2>About Our Community</h2>
+</section>
+<?php
+    include_once 'footer.php';
+?>
