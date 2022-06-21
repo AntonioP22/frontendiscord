5b3ae78549e3c20c22463541bd39b5df4ed18e7b
AntonioP22 <mammothsnowb@gmail.com>
2022-06-21 06:10:41 -0700

    Created projects.php

---
+++ projects.php
@@ -0,0 +1,10 @@
+<?php
+    include_once 'header.php';
+?>
+    <h1>Welcome To The Community Projects Page</h1>
+    <section>
+        <p>Post your projects here</p>
+    </section>
+<?php
+    include_once 'footer.php';
+?>
