a3eb6bc2b048f787640a9e9b19b080f8b2332a54
AntonioP22 <mammothsnowb@gmail.com>
2022-06-21 06:13:29 -0700

    update

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 <footer>
     Coded by the Frontend Discord Community
-    <embed
-      src="https://pngimg.com/uploads/github/github_PNG58.png" title="Click Me"/>
+    <img src="https://pngimg.com/uploads/github/github_PNG58.png" title="Click Me"/>
   </footer>
 </body>
 </html>
