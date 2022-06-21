a052990a96f47b46ee8e9a748be3a29945fef08b
joppe montezinos <34985258+joppe2001@users.noreply.github.com>
2022-06-21 19:52:01 +0200

    changed the footer position

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
 <footer>
+  <p>
     Coded by the Frontend Discord Community
-    <embed src="https://pngimg.com/uploads/github/github_PNG58.png" title="Click Me"/>
-  </footer>
+  </p>
+  <embed src="https://pngimg.com/uploads/github/github_PNG58.png" title="Click Me" />
+</footer>
 </body>
+
 </html>
