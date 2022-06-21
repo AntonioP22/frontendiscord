a623cebe878ad04e95d308aea5ac413fc2ad0b44
joppe montezinos <34985258+joppe2001@users.noreply.github.com>
2022-06-21 23:14:51 +0200

    tesing more shit

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <footer>
   <p>
     Coded by the Frontend Discord Community
-    <?php
+  </p>
+  <embed src="https://pngimg.com/uploads/github/github_PNG58.png" title="Click Me" />
+</footer>
+<?php
 if (isset($_SESSION['userUid'])) {
   echo "<p>You are logged in " . $_SESSION["userUid"] . "</p>";
 }
 ?>
-  </p>
-  <embed src="https://pngimg.com/uploads/github/github_PNG58.png" title="Click Me" />
-</footer>
 </body>
 
 </html>
