71fc00b3f9ff066c02577f2f85891cd9b938da57
joppe montezinos <34985258+joppe2001@users.noreply.github.com>
2022-06-21 23:22:40 +0200

    test commit

--- footer.php
+++ footer.php
@@ -1,14 +1,20 @@
+
+<div class="showLogin">
+  <?php
+if (isset($_SESSION['userUid'])) {
+  echo "<p>You are logged in " . $_SESSION["userUid"] . "</p>";
+}
+?>
+</div>
+
 <footer>
   <p>
     Coded by the Frontend Discord Community
   </p>
   <embed src="https://pngimg.com/uploads/github/github_PNG58.png" title="Click Me" />
+  
 </footer>
-<?php
-if (isset($_SESSION['userUid'])) {
-  echo "<p>You are logged in " . $_SESSION["userUid"] . "</p>";
-}
-?>
+
 </body>
 
 </html>
