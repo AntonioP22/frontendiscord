8430750ebbd553f61d24fcece95e00af777b2f02
joppe montezinos <34985258+joppe2001@users.noreply.github.com>
2022-06-21 19:46:44 +0200

    new colors etc

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
     include_once 'header.php';
 ?>
-    <h1>Frontend Discord Community</h1>
+
         <?php
           if (isset($_SESSION['userUid'])) {
             echo "<p>You are logged in " . $_SESSION["userUid"] . "</p>";
           }
         ?>
-    <section>
+    <section class="mid-section">
       <h2>Welcome to the our open-source project!</h2>
       <article>
         <p>If you'd like to contribute please read GitHub's contributing rules and guidelines. Then reach out with either a pull request or username to become a contributor!</p>
