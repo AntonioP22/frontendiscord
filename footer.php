f66bb638a8c5b479b2593f85b58351b77e3bd0db
AntonioP22 <mammothsnowb@gmail.com>
2022-06-21 06:28:36 -0700

    background update

--- footer.php
+++ footer.php
@@ -3,4 +3,5 @@
     <embed src="https://pngimg.com/uploads/github/github_PNG58.png" title="Click Me"/>
   </footer>
 </body>
+</div>
 </html>
--- header.php
+++ header.php
@@ -19,6 +19,12 @@ session_start();
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/comic-mono@0.0.1/index.css">
 </head>
 
+<div class="background-container">
+<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt="">
+<div class="stars"></div>
+<div class="twinkling"></div>
+<div class="clouds"></div>
+
 <body>
   <nav>
     <ul class="navbar-nav">
@@ -42,3 +48,5 @@ session_start();
       ?>
     </ul>
   </nav>
+
+  
