fd6493a5e2e97897d6b4ec4f5e352d331a4211e0
AntonioP22 <mammothsnowb@gmail.com>
2022-06-23 10:46:26 -0700

    updated discover.php

--- discover.php
+++ discover.php
@@ -4,6 +4,17 @@
 <section>
   <h2>About Our Community</h2>
 </section>
+<div>
+  <article>
+    <p>Welcome!!!
+This will be a place for all of us beginners and beyond.
+You can share whatever you want in this group.
+If you like,  introduce yourself with your names, nationalities and your current progress in the front end course. As well as what you hope to get in the group.
+Our goal in this group is to share experiences and support each other who are just starting the world of programming.
+It will be a pleasure to get to know you all!</p>
+<p>Add or edit here what u think describes this server the best</p>
+  </article>
+</div>
 <?php
     include_once 'footer.php';
 ?>
