1229588d7c1b18a34c4fa5aeabae03ade2c28c6f
joppe montezinos <34985258+joppe2001@users.noreply.github.com>
2022-06-23 22:24:05 +0200

    testing

--- projects.php
+++ projects.php
@@ -6,59 +6,57 @@ include_once 'header.php';
     <section>
         <h2>Post your projects here</h2>
     </section>
-    <section>
-        <div class="main-container">
-            <div class="intro">
-                <ul class="project-list-intro">
-                    <li>This is the place to add your projects.</li>
-                    <li>People can then take a look at them.</li>
-                    <li>We will continue to addd to this page.</li>
+    <div class="main-container">
+        <div class="intro">
+            <ul class="project-list-intro">
+                <li><p>&#9775;</p>This is the place to add your projects.</li>
+                <li>People can then take a look at them.</li>
+                <li>We will continue to addd to this page.</li>
+            </ul>
+        </div>
+        <div class="main-project-page">
+            <div class="project1">
+                <ul>
+                    <li>Project 1</li>
                 </ul>
             </div>
-            <div class="main-project-page">
-                <div class="project1">
-                    <ul>
-                        <li>Project 1</li>
-                    </ul>
-                </div>
-                <div class="project2">
-                    <ul>
-                        <li>Project 1</li>
-                    </ul>
-                </div>
-                <div class="project3">
-                    <ul>
-                        <li>Project 1</li>
-                    </ul>
-                </div>
-                <div class="project4">
-                    <ul>
-                        <li>Project 1</li>
-                    </ul>
-                </div>
-                <div class="project5">
-                    <ul>
-                        <li>Project 1</li>
-                    </ul>
-                </div>
-                <div class="project6">
-                    <ul>
-                        <li>Project 1</li>
-                    </ul>
-                </div>
-                <div class="project7">
-                    <ul>
-                        <li>Project 1</li>
-                    </ul>
-                </div>
-                <div class="project8">
-                    <ul>
-                        <li>Project 1</li>
-                    </ul>
-                </div>
+            <div class="project2">
+                <ul>
+                    <li>Project 1</li>
+                </ul>
+            </div>
+            <div class="project3">
+                <ul>
+                    <li>Project 1</li>
+                </ul>
+            </div>
+            <div class="project4">
+                <ul>
+                    <li>Project 1</li>
+                </ul>
+            </div>
+            <div class="project5">
+                <ul>
+                    <li>Project 1</li>
+                </ul>
+            </div>
+            <div class="project6">
+                <ul>
+                    <li>Project 1</li>
+                </ul>
+            </div>
+            <div class="project7">
+                <ul>
+                    <li>Project 1</li>
+                </ul>
+            </div>
+            <div class="project8">
+                <ul>
+                    <li>Project 1</li>
+                </ul>
             </div>
         </div>
-    </section>
+    </div>
 </div>
 
 <?php
