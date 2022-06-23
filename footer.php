798bb192ec86d3d90626aeb5089ad5c43fda54d6
AntonioP22 <mammothsnowb@gmail.com>
2022-06-23 11:41:45 -0700

    remove link

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
   <p>
     Coded by the Frontend Discord Community
   </p>
-  <a href="https://github.com/AntonioP22/frontendiscord"><embed src="https://pngimg.com/uploads/github/github_PNG58.png" title="Click Me" /></a>
+  <embed src="https://pngimg.com/uploads/github/github_PNG58.png" title="Click Me" />
   
 </footer>
 
