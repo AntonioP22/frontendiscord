d30ce5c3a403404d3881547d45d2ddaf3605802d
joppe montezinos <34985258+joppe2001@users.noreply.github.com>
2022-06-23 21:59:32 +0200

    testing

--- projects.php
+++ projects.php
@@ -1,63 +1,65 @@
 <?php
 include_once 'header.php';
 ?>
-<h1>Welcome To The Community Projects Page</h1>
-<section>
-    <h2>Post your projects here</h2>
-</section>
-<section>
-    <div class="main-container">
-        <div class="intro">
-            <ul class="project-list-intro">
-                <li>This is the place to add your projects.</li>
-                <li>People can then take a look at them.</li>
-                <li>We will continue to addd to this page.</li>
-            </ul>
+<div class="all-around">
+    <h1>Welcome To The Community Projects Page</h1>
+    <section>
+        <h2>Post your projects here</h2>
+    </section>
+    <section>
+        <div class="main-container">
+            <div class="intro">
+                <ul class="project-list-intro">
+                    <li>This is the place to add your projects.</li>
+                    <li>People can then take a look at them.</li>
+                    <li>We will continue to addd to this page.</li>
+                </ul>
+            </div>
+            <div class="main-project-page">
+                <div class="project1">
+                    <ul>
+                        <li>Project 1</li>
+                    </ul>
+                </div>
+                <div class="project2">
+                    <ul>
+                        <li>Project 1</li>
+                    </ul>
+                </div>
+                <div class="project3">
+                    <ul>
+                        <li>Project 1</li>
+                    </ul>
+                </div>
+                <div class="project4">
+                    <ul>
+                        <li>Project 1</li>
+                    </ul>
+                </div>
+                <div class="project5">
+                    <ul>
+                        <li>Project 1</li>
+                    </ul>
+                </div>
+                <div class="project6">
+                    <ul>
+                        <li>Project 1</li>
+                    </ul>
+                </div>
+                <div class="project7">
+                    <ul>
+                        <li>Project 1</li>
+                    </ul>
+                </div>
+                <div class="project8">
+                    <ul>
+                        <li>Project 1</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div class="main-project-page">
-            <div class="project1">
-                <ul>
-                    <li>Project 1</li>
-                </ul>
-            </div>
-            <div class="project2">
-                <ul>
-                    <li>Project 1</li>
-                </ul>
-            </div>
-            <div class="project3">
-                <ul>
-                    <li>Project 1</li>
-                </ul>
-            </div>
-            <div class="project4">
-                <ul>
-                    <li>Project 1</li>
-                </ul>
-            </div>
-            <div class="project5">
-                <ul>
-                    <li>Project 1</li>
-                </ul>
-            </div>
-            <div class="project6">
-                <ul>
-                    <li>Project 1</li>
-                </ul>
-            </div>
-            <div class="project7">
-                <ul>
-                    <li>Project 1</li>
-                </ul>
-            </div>
-            <div class="project8">
-                <ul>
-                    <li>Project 1</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</section>
+    </section>
+</div>
 
 <?php
 include_once 'footer.php';
