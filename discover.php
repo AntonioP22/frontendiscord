fcd21b96acc377d788d25d241a7750b59b7955e3
AntonioP22 <mammothsnowb@gmail.com>
2022-06-23 10:57:56 -0700

    add css class to discover.php

--- discover.php
+++ discover.php
@@ -1,10 +1,11 @@
 <?php
     include_once 'header.php';
 ?>
-<section>
+<div class="l-container">
+<section class="about-section">
   <h2>About Our Community</h2>
 </section>
-<div>
+<div class="welcome-section">
   <article>
     <p>Welcome!!!
 This will be a place for all of us beginners and beyond.
@@ -14,6 +15,7 @@ Our goal in this group is to share experiences and support each other who are ju
 It will be a pleasure to get to know you all!</p>
 <p>Add or edit here what u think describes this server the best</p>
   </article>
+</div>
 </div>
 <?php
     include_once 'footer.php';
