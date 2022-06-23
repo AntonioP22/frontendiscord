73a8c05a080584254d95b09a28edb3e5b35d96f5
joppe montezinos <34985258+joppe2001@users.noreply.github.com>
2022-06-23 19:05:14 +0200

    test leaderboard

--- index.php
+++ index.php
@@ -11,6 +11,27 @@ include_once 'header.php';
       <p>If you'd like to contribute please read GitHub's contributing rules and guidelines. Then reach out with either a pull request or username to become a contributor!</p>
     </article>
   </div>
+  <div class="leaderboard-container">
+    <div class="row">
+        <div class="name">Player1</div><div class="score">430</div>
+    </div>
+    
+    <div class="row">
+        <div class="name">Player2</div><div class="score">580</div>
+    </div>
+    
+    <div class="row">
+        <div class="name">Player3</div><div class="score">310</div>
+    </div>
+    
+    <div class="row">
+        <div class="name">Player4</div><div class="score">640</div>
+    </div>
+    
+    <div class="row">
+        <div class="name">Player5</div><div class="score">495</div>
+    </div>
+</div>
 </div>
 <?php
 include_once 'footer.php';
