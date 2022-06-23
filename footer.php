a545c931fd279f8039f74f173bbe535151ad9339
AntonioP22 <mammothsnowb@gmail.com>
2022-06-23 11:40:24 -0700

    added link to github repo

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
   <p>
     Coded by the Frontend Discord Community
   </p>
-  <embed src="https://pngimg.com/uploads/github/github_PNG58.png" title="Click Me" />
+  <a href="https://github.com/AntonioP22/frontendiscord"><embed src="https://pngimg.com/uploads/github/github_PNG58.png" title="Click Me" /></a>
   
 </footer>
 
