1c5cba9492b867c72b34e657cf51877a0988388c
joppe montezinos <34985258+joppe2001@users.noreply.github.com>
2022-06-23 18:31:13 +0200

    trying som egrid things

--- index.php
+++ index.php
@@ -2,17 +2,16 @@
 include_once 'header.php';
 ?>
 
-
-<section class="mid-section">
-
-  <h2>Welcome to the our open-source project!</h2>
-  <article>
-    <p>If you'd like to contribute please read GitHub's contributing rules and guidelines. Then reach out with either a pull request or username to become a contributor!</p>
-
-  </article>
-  
-</section>
-
+<div class="large-container">
+  <section class="welcome-section">
+    <h2>Welcome to the our open-source project!</h2>
+  </section>
+  <div class="introduction-section">
+    <article>
+      <p>If you'd like to contribute please read GitHub's contributing rules and guidelines. Then reach out with either a pull request or username to become a contributor!</p>
+    </article>
+  </div>
+</div>
 <?php
 include_once 'footer.php';
 ?>
